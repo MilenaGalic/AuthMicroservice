b216f6013fb30208938f1078e2eddfa02cc712fa
Milan Latinovic <milan.softline@gmail.com>
2017-09-11 17:39:09 +0200

    Basic authentication with Lumen and JWT implemented

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+        	'email' => 'example1@milanlatinovic.com',
+        	'password' => app('hash')->make('1234')
+        ]);
+
+        factory(App\User::class)->create([
+        	'email' => 'milan.softline@gmail.com',
+        	'password' => app('hash')->make('admin')
+        ]);
+
+        factory(App\User::class)->create([
+        	'email' => 'example3@google.com',
+        	'password' => app('hash')->make('1234')
+        ]);
+
+    }
+}
